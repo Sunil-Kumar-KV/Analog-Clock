ec424ed5031ba0a71db7cdca8773500fbdd31a0c
Sunil Kumar K V <111721933+Sunil-Kumar-KV@users.noreply.github.com>
2023-03-17 08:18:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,11 @@ modeSwitch.addEventListener("click", () => {
   // set localStorage "mode" item based on "dark" class presence
   localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
 });
+
+const updateTime = () => {
+  // Get current time and calculate degrees for clock hands
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
