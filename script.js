f87c63e68163d321a766aa35ab2cd038925d8448
Sunil Kumar K V <111721933+Sunil-Kumar-KV@users.noreply.github.com>
2023-03-20 08:36:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,20 @@ const updateTime = () => {
   hourHand.style.transform = `rotate(${hrToDeg}deg)`;
 };
 
+cloneTicks();	
+setInterval(setTime, 1000);
+
+speaker.onclick = function(e) {
+	e.preventDefault;
+
+	if (min === 0) {
+	min = hour;
+	hour = 'Even';
+	}
+	if (min < 10 && min > 1) {
+	min = 'o' + min;
+	}
+
 
 // call updateTime to set clock hands every second
 setInterval(updateTime, 1000);
